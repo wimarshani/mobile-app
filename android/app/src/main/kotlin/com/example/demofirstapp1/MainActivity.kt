57469dfa85bbb9710e3a3561584249f0b6966ed5
wimarshani <97798984+wimarshani@users.noreply.github.com>
2023-07-13 21:33:14 +0530

    uplord demoapp1

---
+++ android/app/src/main/kotlin/com/example/demofirstapp1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demofirstapp1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Wimarshani")),
+          elevation: 4,
+        ),
+        body: const Center(child: Text("Hii Welcome")));
+  }
+}
